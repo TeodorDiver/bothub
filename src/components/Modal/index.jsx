c80297169db281e014ae2a173deddf71ac940231
Sergey Satarov <160714602+TeodorDiver@users.noreply.github.com>
2024-08-12 14:58:17 +0400

    Update index.jsx

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -4,7 +4,7 @@ import { OpenAIApi, Configuration } from 'openai-edge';
 
 
 const configuration = new Configuration({
-  apiKey: "*********************************************************************************************************************************************************************************************",
+  apiKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY5MGE2ZTgxLTRiMDMtNGQxNC1hMGQxLWI3N2RkZjlkMDY2ZiIsImlzRGV2ZWxvcGVyIjp0cnVlLCJpYXQiOjE3MjA1Mjk0NDgsImV4cCI6MjAzNjEwNTQ0OH0.Dm8QJpXfX2ChWcYZ5c0SLNzGpmEmh1dYPAMW3wz4v5M",
   basePath: "https://bothub.chat/api/v2/openai/v1",
 });
 const openai = new OpenAIApi(configuration);
